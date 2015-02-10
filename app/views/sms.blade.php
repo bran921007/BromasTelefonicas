d60376dd4a44bd48cedc6aec3241267fdf99fda5
Jhonnatan Rodriguez <jhonn@outlook.com>
2015-02-10 08:19:00 -0400

    Create sms.blade.php

---
+++ app/views/sms.blade.php
@@ -0,0 +1,176 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="#">
+
+  <title>SMS Voice Free</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <style media="screen">
+  /* Sticky footer styles
+  -------------------------------------------------- */
+  html {
+    position: relative;
+    min-height: 100%;
+  }
+  body {
+    /* Margin bottom by footer height */
+    margin-bottom: 60px;
+  }
+  .footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    /* Set the fixed height of the footer here */
+    height: 60px;
+    background-color: #f5f5f5;
+  }
+
+
+  /* Custom page CSS
+  -------------------------------------------------- */
+  /* Not required for template or sticky footer method. */
+
+  body > .container {
+    padding: 60px 15px 0;
+  }
+  .container .text-muted {
+    margin: 20px 0;
+  }
+
+  .footer > .container {
+    padding-right: 15px;
+    padding-left: 15px;
+  }
+
+  code {
+    font-size: 80%;
+    }
+
+    .align-center{
+      margin-top: 150px;
+    }
+
+    .panel-title{
+      padding-top: 10px;
+      padding-bottom: 0px;
+      font-size: 20px;
+      font-weight: 300;
+      color: #777;
+
+    }
+
+    .panel{
+      font-size: 20px;
+      font-weight: 300;
+      color: #777;
+      border: 1px solid #d8d8d8 !important;
+      text-align: center;
+    }
+
+    .card-footer{
+      padding: 11px;
+      margin: 0;
+      background-color: #f8f8f8;
+    }
+
+  </style>
+
+  <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+  <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+  <script src="http://getbootstrap.com/assets/js/ie-emulation-modes-warning.js"></script>
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+
+  <!-- Fixed navbar -->
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">SMS Voice Free</a>
+      </div>
+      <div id="navbar" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home</a></li>
+          <!-- <li><a href="#about">About</a></li> -->
+
+
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </nav>
+
+  <!-- Begin page content -->
+  <div class="container">
+
+
+
+    <div class="row">
+    <div class="col-md-4">
+    </div>
+  <div class="col-md-4 ">
+    <div class="panel panel-default align-center" style="border-radius: 0px;">
+      <div class="panel-heading" style="background-color: #ffffff;">
+        <h3 class="panel-title">Personaliza Mensaje</h3>
+      </div>
+      <div class="panel-body" >
+        <div class="form-group col-md-9">
+          <label class="sr-only" for="exampleInputAmount"></label>
+          <div class="input-group">
+            <div class="input-group-addon">+</div>
+            <input type="text" class="form-control" id="exampleInputAmount" placeholder="1 (555) 555-5555">
+          </div>
+        </div>
+      </div>
+      <div class="card-footer">
+        <button class="btn btn-success btn-sm" type="submit">Enviar Mensaje</button>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+  </div>
+</div>
+
+
+
+  </div>
+
+
+
+  <footer class="footer">
+    <div class="container">
+      <p class="text-muted">SMS Voice Free © 2015 All Rights Reserved.</p>
+    </div>
+  </footer>
+
+
+  <!-- Bootstrap core JavaScript
+  ================================================== -->
+  <!-- Placed at the end of the document so the pages load faster -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+  <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+  <script src="http://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
